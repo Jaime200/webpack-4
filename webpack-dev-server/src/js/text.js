4a3877a6d84c2d73f1323ac62f68968a026796aa
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-10-30 12:16:02 -0600

    Se realizan cambios en css y js para testear las configuraciones

--- webpack-dev-server/src/js/text.js
+++ webpack-dev-server/src/js/text.js
@@ -1,5 +1,5 @@
 function text() {
-    document.body.innerHTML = 'Hola mundo desde webPack dev server watch 200 sin '
+    document.body.innerHTML = 'Hola mundo desde webPack dev server watch 200 hot reload'
 }
 
 export default text;
