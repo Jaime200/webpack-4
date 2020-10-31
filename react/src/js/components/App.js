3603a3c60655b126da7e860e8739bf6126f65ce5
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-10-31 13:10:59 -0600

    Se crean componentes de react

--- react/src/js/components/App.js
+++ react/src/js/components/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import data from './data.json'
+import Loader from './loader'
+console.log(data);
 function App(){
+    const [loaderList, setLoaderList] = useState([]);
+    function handleClick(){
+        setLoaderList(data.loaders);
+    }
     return (
         <div>
-            Que linda aplicacion con React JS
+            <p>Que linda aplicacion con React JS</p>
+            <ul>
+                {
+                    loaderList.map((item)=> <Loader key={item.id} {...item}/>)
+                    
+                }
+            </ul>
+            <button onClick={handleClick} >Mostrar loader</button>
+            
         </div>
     )
 }
