28c9b5b2f2ab5ca1770f553cc6743d3c8207b5c8
Jaime Muñoz <jaime.munoz200@gmail.com>
2020-11-01 11:48:49 -0600

    Se agrega carpeta para optimizacion de chunks

---
+++ prevent-duplication/src/js/components/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import data from './data.json'
+import Loader from './loader'
+import logo from '../../images/platzi.png'
+import video from '../../video/que-es-core.mp4'
+import '../../less/less.less'
+import '../../sass/sass.scss'
+import '../../stylus/stylus.styl'
+
+console.log(data);
+function App(){
+    const [loaderList, setLoaderList] = useState([]);
+    function handleClick(){
+        setLoaderList(data.loaders);
+    }
+    return (
+        <div>\
+           <p className="sass">Esto es sass</p>
+           <p className="less">Esto es less</p>
+           <p className="stylus">Esto es stylus</p>
+           <p className="post-css">Esto es postCSS</p>
+            <p>Que linda aplicacion con React JS img loader</p>
+            <video src={video} width={360} controls poster={logo}></video>
+            <p>
+                <img src={logo} alt=""  width={40} />
+            </p>
+            <ul>
+                {
+                    loaderList.map((item)=> <Loader key={item.id} {...item}/>)
+                    
+                }
+            </ul>
+            <button onClick={handleClick} >Mostrar loader</button>
+            
+        </div>
+    )
+}
+
+
+
+export default App
